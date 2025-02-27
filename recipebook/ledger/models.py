7b88902a01c0aee9ed692a5dade9023c6182d0ed
hannahdenisebacomo@gmail.com <hannahdenisebacomo@gmail.com>
2025-02-27 18:50:44 +0800

    Added Recipe, Ingredient, and RecipeIngredient models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient_detail', args=[str(self.id)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args=[str(self.id)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE,
+        null = True,
+        related_name='ingredient')
+    
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        null = True,
+        related_name='recipe')
+    
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
