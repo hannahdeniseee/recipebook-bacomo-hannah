31636e2e58ac0b7dcee269066bd7532e3e1433dd
hannahdenisebacomo@gmail.com <hannahdenisebacomo@gmail.com>
2025-03-10 19:30:49 +0800

    Updated models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 
 
 class Ingredient(models.Model):
@@ -12,8 +14,11 @@ class Ingredient(models.Model):
         return reverse('ledger:ingredient-detail', args=[str(self.id)])
 
 
-class Recipe(models.Model):
+class Recipe(models.Model): 
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    update_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -29,10 +34,19 @@ class RecipeIngredient(models.Model):
         Ingredient, 
         on_delete=models.CASCADE,
         related_name='recipe')
-    
+  
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE,
         related_name='ingredients')
-    
+   
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
+    
+    
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,6 +4,6 @@ from .views import RecipeListView, RecipeDetailView
 app_name = 'ledger'
 
 urlpatterns = [
-    path('list', RecipeListView.as_view(), name='recipe-list'),
+    path('list/', RecipeListView.as_view(), name='recipe-list'),
     path('<int:pk>/detail', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
